a90d6634e9e2372e7dd1c6a307ac7c18789ff1df
IanAquino <ianpedroaquino@gmail.com>
2021-04-21 12:19:13 +0100

    mostrar mensagem

--- app/src/main/java/com/example/mensagem/MostraMensagemActivity2.kt
+++ app/src/main/java/com/example/mensagem/MostraMensagemActivity2.kt
@@ -2,10 +2,17 @@ package com.example.mensagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MostraMensagemActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem2)
+
+        val mensagem = intent.getStringExtra("MENSAGEM")
+
+        val textViewMensagem = findViewById<TextView>(R.id.textViewMesagem)
+
+        textViewMensagem.text = mensagem
     }
 }
