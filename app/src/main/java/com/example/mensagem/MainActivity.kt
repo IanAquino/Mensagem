0b5a97c467315db83eb2bbd6f34e3a7c1bcd261b
IanAquino <ianpedroaquino@gmail.com>
2021-04-23 13:19:03 +0100

    mostrar data/hora !

--- app/src/main/java/com/example/mensagem/MainActivity.kt
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mensagem
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import java.util.*
 import kotlin.time.measureTimedValue
 
@@ -16,10 +21,15 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun enviaMensagem(view: View) {
         val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
 
         val mensagem = editTextMensagem.text.toString()
+
+        val date = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        val formatted = date.format(formatter)
 
         if(mensagem.isBlank()){
             editTextMensagem.error = getString(R.string.mensagem_vazia)
@@ -27,21 +37,19 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-       /* val date = Calendar.getInstance().time
-        val tvFullDateTime.text = date.toString()
-        var dateTimeFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.getDefault())
-        val tvDateTime.text = dateTimeFormat.format(date)*/
-
-
 
         val intent = Intent(this, MostraMensagemActivity2::class.java).apply {  }
         intent.putExtra("MENSAGEM", mensagem)
+        intent.putExtra("DATA", formatted)
         startActivity(intent)
+
     }
 
     companion object {
         const val INFO_EXTRA_MENSAGEM = "MENSAGEM"
+        const val INFO_EXTRA_TIME = "DATA"
     }
+
 
 }
 
--- app/src/main/java/com/example/mensagem/MostraMensagemActivity2.kt
+++ app/src/main/java/com/example/mensagem/MostraMensagemActivity2.kt
@@ -1,24 +1,30 @@
 package com.example.mensagem
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MostraMensagemActivity2 : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem2)
 
-      //  val mensagem = intent.getStringExtra("MENSAGEM")
+       // val mensagem = intent.getStringExtra("MENSAGEM")
 
         val mensagem = intent.getStringExtra((MainActivity.INFO_EXTRA_MENSAGEM))
+        val date = intent.getStringExtra((MainActivity.INFO_EXTRA_TIME))
 
         val textViewMensagem = findViewById<TextView>(R.id.textViewMesagem)
 
+        val textViewDataHora = findViewById<TextView>(R.id.textViewDataHora)
 
+        textViewDataHora.text = date
         textViewMensagem.text = mensagem
-
-      //  val tvDataTime.text = intent.getStringExtra((MainActivity.INFO_EXTRA_TIME))
 
     }
 }
